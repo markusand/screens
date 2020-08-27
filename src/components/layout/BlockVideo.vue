9d02ff483e0b5b229196e52d620e6e6a6e451141
markusand <mvilella@obsa.ad>
2020-08-27 12:53:05 +0200

    Add layout components

---
+++ src/components/layout/BlockVideo.vue
@@ -0,0 +1,42 @@
+<template>
+	<container :tag="tag" class="block-video">
+		<h2
+			class="block-video__title"
+			data-animate="reveal-down"
+			data-delay-in="300">
+			{{ title }}
+		</h2>
+		<div class="block-video__video">
+			<div
+				v-if="shadow"
+				class="shadow shadow--bl"
+				data-animate="reveal-right"
+				data-delay-in="600" />
+			<video-embed
+				v-bind="$attrs"
+				:src="src"
+				:subs="$i18n.locale"
+				data-animate="reveal-right" />
+		</div>
+		<p data-animate="fade-up" data-delay="300">
+			<slot />
+		</p>
+	</container>
+</template>
+
+<script>
+import Container from '@/components/layout/Container.vue';
+import VideoEmbed from '@/components/VideoEmbed.vue';
+
+export default {
+	name: 'BlockVideo',
+	components: { Container, VideoEmbed },
+	inheritAttrs: false,
+	props: {
+		src: { type: String, required: true },
+		title: { type: String, default: null },
+		tag: { type: String, default: null },
+		shadow: { type: Boolean, default: false },
+	},
+};
+</script>
---
+++ src/components/layout/index.js
@@ -0,0 +1,5 @@
+import Container from './Container.vue';
+import BlockVideo from './BlockVideo.vue';
+import BlockImage from './BlockImage.vue';
+
+export { Container, BlockVideo, BlockImage };
