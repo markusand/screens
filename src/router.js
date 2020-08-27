7ed7f4fd70660ffc56d0af463056d5444fd645f2
markusand <mvilella@obsa.ad>
2020-08-27 13:04:45 +0200

    Add About page
    [+] Include ImageMarkers component

--- src/router.js
+++ src/router.js
@@ -14,6 +14,11 @@ export default new Router({
 			component: Home,
 		},
 		{
+			path: '/about',
+			name: 'about',
+			component: () => import('./views/About.vue'),
+		},
+		{
 			path: '*',
 			name: 'NotFound',
 			component: () => import('./views/NotFound.vue'),
---
+++ src/views/About.vue
@@ -0,0 +1,47 @@
+<template>
+	<main id="page-about">
+		<!-- Project vision -->
+		<block-video
+			:src="introVideo"
+			title="Casa de la Muntanya"
+			:tag="$t('about.vision')"
+			shadow>
+			{{ $t('about.our_vision') }}
+		</block-video>
+
+		<!-- CdM floorplan -->
+		<container :tag="$t('about.floorplan')" dual-tag class="fill">
+			<image-markers :markers="markers" class="floorplan-markers">
+				<img svg-inline src="@/assets/vectors/floorplan.svg">
+				<div
+					slot="marker"
+					slot-scope="{ marker, i }"
+					data-animate="fade-down"
+					:data-delay-in="1000 + 150 * i"
+					:data-tooltip="$t(`about.zones.${marker.id}`)">
+					<i :class="marker.icon" />
+				</div>
+			</image-markers>
+		</container>
+	</main>
+</template>
+
+<script>
+import TransitionsViews from '@/utils/mixin.transitions-views';
+import { Container, BlockVideo } from '@/components/layout';
+import ImageMarkers from '@/components/ImageMarkers.vue';
+
+import { introVideo, markers } from '@/config/about';
+
+export default {
+	name: 'Home',
+	components: { Container, BlockVideo, ImageMarkers },
+	mixins: [TransitionsViews],
+	data() {
+		return {
+			introVideo,
+			markers,
+		};
+	},
+};
+</script>
