f7d74b8519e64fecf1d8afd4f457eb53e0bc8a40
markusand <mvilella@obsa.ad>
2020-08-27 12:50:50 +0200

    Add language navigation

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,17 @@
 			<router-link to="/">
 				<img svg-inline src="@/assets/logos/cdm.svg" class="logo-cdm">
 			</router-link>
-			<section />
+			<section>
+				<ul class="nav-languages">
+					<li v-for="language in $i18n.availableLocales" :key="language">
+						<a :href="`#${language}`"
+							:class="{ 'active': language === $i18n.locale }"
+							@click.prevent="$i18n.locale = language">
+							{{ language }}
+						</a>
+					</li>
+				</ul>
+			</section>
 		</header>
 		<router-view id="app__content" />
 	</div>
