eddda7a01b649d60945b5a482b57775e7f02d924
markusand <mvilella@obsa.ad>
2020-09-29 18:57:13 +0200

    Decouple business logic and content text
    Load i18n translations from external source (another Github repo)

--- src/i18n.js
+++ src/i18n.js
@@ -1,14 +1,23 @@
 import Vue from 'vue';
 import Vuei18n from 'vue-i18n';
-
-import ca from './locales/ca';
+import axios from 'axios';
 
 Vue.use(Vuei18n);
 
-const messages = { ca };
+const languages = ['ca', 'en'];
 
-export default new Vuei18n({
+const i18n = new Vuei18n({
 	locale: 'ca',
 	fallbackLocale: 'ca',
-	messages,
+	silentTranslationWarn: process.env.NODE_ENV === 'production',
+	messages: {},
 });
+
+languages.forEach(code => {
+	const url = `${process.env.VUE_APP_LOCALES_URL}${code}.json`;
+	axios.get(url).then(({ data }) => {
+		i18n.setLocaleMessage(code, data);
+	});
+});
+
+export default i18n;
